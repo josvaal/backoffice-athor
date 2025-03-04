66381d361799d336dd6c6c8ea96f003ca9e76c19
José Valentino <josvalmtrz@gmail.com>
2025-03-04 11:53:02 -0500

    feat: Swagger modo oscuro

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const theme = new SwaggerTheme();
+
+  const config = new DocumentBuilder()
+    .setTitle('Backoffice Athor')
+    .setDescription('API del backoffice de Athor')
+    .setVersion('1.0')
+    // .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory, {
+    customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK),
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
