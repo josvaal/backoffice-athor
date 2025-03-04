b67cbfbd01c375d23ec41a34ae9f57f485cc692a
José Valentino <josvalmtrz@gmail.com>
2025-03-04 11:50:58 -0500

    feat: mejor manejo de errores y tipado con el modulo auth

--- backend/src/auth/auth.guard.ts
+++ backend/src/auth/auth.guard.ts
@@ -25,8 +25,10 @@ export class AuthGuard implements CanActivate {
       // 💡 We're assigning the payload to the request object here
       // so that we can access it in our route handlers
       request['user'] = payload;
-    } catch {
-      throw new UnauthorizedException();
+    } catch (error) {
+      throw new UnauthorizedException(
+        'No tienes acceso a esto, consulta a un administrador o de rol más alto',
+      );
     }
     return true;
   }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import {
   Injectable,
+  NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
@@ -7,7 +8,12 @@ import { User } from '@prisma/client';
 import { UserSignupDto } from 'src/users/dto/user-signup.dto';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
-import { UserUpdateDto } from 'src/users/dto/user-update.dto';
+import {
+  ApiResponse,
+  JwtPayload,
+  JwtRequestPayload,
+  JwtToken,
+} from 'src/custom.types';
 
 @Injectable()
 export class AuthService {
@@ -16,27 +22,85 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signIn(email: string, pass: string): Promise<any> {
-    const user: User | null = await this.usersService.findByEmail(email);
+  async signIn(email: string, pass: string): Promise<ApiResponse<JwtToken>> {
+    try {
+      const user: User | null = await this.usersService.findByEmail(email);
 
-    const isMatch = await bcrypt.compare(pass, user?.password as string);
-    if (!isMatch) {
-      throw new UnauthorizedException();
+      const isMatch = await bcrypt.compare(pass, user?.password as string);
+      if (!isMatch) {
+        throw new UnauthorizedException();
+      }
+
+      const payload: JwtPayload = {
+        sub: user?.id.toString() as string,
+        user: user as User,
+      };
+
+      return {
+        error: null,
+        data: {
+          access_token: await this.jwtService.signAsync(payload),
+          refresh_token: await this.jwtService.signAsync(
+            { sub: user?.id },
+            { expiresIn: '7d' },
+          ),
+        },
+      };
+    } catch (error) {
+      return {
+        error: error,
+        data: null,
+      };
     }
-
-    const payload = { sub: user?.id, email: user?.email };
-
-    return {
-      access_token: await this.jwtService.signAsync(payload),
-    };
   }
 
-  async signUp(userDto: UserSignupDto): Promise<any> {
-    const user: User | null = await this.usersService.create(userDto);
-    const payload = { sub: user?.id, email: user?.email };
+  async signUp(userDto: UserSignupDto): Promise<ApiResponse<JwtToken>> {
+    try {
+      const user = await this.usersService.create(userDto);
+      const payload: JwtPayload = {
+        sub: user?.id.toString() as string,
+        user: user as User,
+      };
 
-    return {
-      access_token: await this.jwtService.signAsync(payload),
-    };
+      return {
+        error: null,
+        data: {
+          access_token: await this.jwtService.signAsync(payload),
+          refresh_token: await this.jwtService.signAsync(
+            { sub: user?.id },
+            { expiresIn: '7d' },
+          ),
+        },
+      };
+    } catch (error) {
+      return {
+        error: error,
+        data: null,
+      };
+    }
+  }
+
+  async profile(request: JwtRequestPayload): Promise<ApiResponse<User>> {
+    try {
+      console.log(request);
+      const userJwt: User = request.user.user;
+      const userDb: User | null = await this.usersService.findById(userJwt.id);
+
+      if (!userDb) {
+        throw new NotFoundException(
+          `El usuario con el id #${userJwt.id} no existe`,
+        );
+      }
+
+      return {
+        error: null,
+        data: userDb,
+      };
+    } catch (error) {
+      return {
+        error: error,
+        data: null,
+      };
+    }
   }
 }
