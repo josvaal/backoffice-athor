e0f489277e4c6308973ad8c0a34c447bb1ca0e15
José Valentino <josvalmtrz@gmail.com>
2025-03-06 12:51:57 -0500

    feat: metodo crud en modelo de dispositivo

--- backend/src/device-model/device-model.controller.ts
+++ backend/src/device-model/device-model.controller.ts
@@ -1,4 +1,170 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { DeviceModelService } from './device-model.service';
+import { RolesWithDescription } from 'decorators/rolesWithDescription.decorator';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { RoleGuard } from 'src/auth/role/role.guard';
+import { ApiResponse } from 'src/custom.types';
+import { CreateModelDeviceDto } from './dto/create-model-device.dto';
+import {
+  PrismaClientKnownRequestError,
+  PrismaClientValidationError,
+} from '@prisma/client/runtime/library';
+import { UpdateModelDeviceDto } from './dto/update-model-device.dto';
 
 @Controller('device-model')
-export class DeviceModelController {}
+export class DeviceModelController {
+  constructor(private deviceModelService: DeviceModelService) {}
+
+  @RolesWithDescription(['superadmin'], '')
+  @UseGuards(AuthGuard, RoleGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get('list')
+  async listAll(): Promise<ApiResponse> {
+    try {
+      return {
+        data: await this.deviceModelService.findAll(),
+        error: null,
+      };
+    } catch (error) {
+      return {
+        error,
+        data: null,
+      };
+    }
+  }
+
+  @RolesWithDescription(['superadmin'], '')
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get('/:id')
+  async listById(@Param('id') id: number): Promise<ApiResponse> {
+    try {
+      return {
+        data: await this.deviceModelService.findById(id),
+        error: null,
+      };
+    } catch (error) {
+      return {
+        error: error,
+        data: null,
+      };
+    }
+  }
+
+  @RolesWithDescription(['superadmin'], '')
+  @UseGuards(AuthGuard, RoleGuard)
+  @HttpCode(HttpStatus.CREATED)
+  @Post('create')
+  async create(
+    @Body() createModelDeviceDto: CreateModelDeviceDto,
+  ): Promise<ApiResponse> {
+    try {
+      return {
+        data: await this.deviceModelService.create(createModelDeviceDto),
+        error: null,
+      };
+    } catch (error) {
+      if (error instanceof PrismaClientValidationError) {
+        return {
+          error: new BadRequestException('Error en los datos enviados'),
+          data: null,
+        };
+      }
+
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        return {
+          error: new BadRequestException('El nombre de serie debe ser único'),
+          data: null,
+        };
+      }
+      return {
+        error,
+        data: null,
+      };
+    }
+  }
+
+  @RolesWithDescription(['superadmin'], '')
+  @UseGuards(AuthGuard, RoleGuard)
+  @HttpCode(HttpStatus.CREATED)
+  @Put('update/:id')
+  async update(
+    @Param('id') id: number,
+    @Body() updateModelDeviceDto: UpdateModelDeviceDto,
+  ): Promise<ApiResponse> {
+    try {
+      return {
+        data: await this.deviceModelService.update(id, updateModelDeviceDto),
+        error: null,
+      };
+    } catch (error) {
+      if (error instanceof PrismaClientValidationError) {
+        return {
+          error: new BadRequestException('Error en los datos enviados'),
+          data: null,
+        };
+      }
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2025'
+      ) {
+        return {
+          error: new NotFoundException(
+            `El modelo de dispositivo con ID #${id} no existe`,
+          ),
+          data: null,
+        };
+      }
+
+      return {
+        error,
+        data: null,
+      };
+    }
+  }
+
+  @RolesWithDescription(['superadmin', 'admin'], '')
+  @UseGuards(AuthGuard, RoleGuard)
+  @HttpCode(HttpStatus.OK)
+  @Delete('delete/:id')
+  async delete(@Param('id') id: number): Promise<ApiResponse> {
+    try {
+      return {
+        data: await this.deviceModelService.delete(id),
+        error: null,
+      };
+    } catch (error) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2025'
+      ) {
+        return {
+          error: new NotFoundException(
+            `El modelo de dispositivo con ID #${id} no existe`,
+          ),
+          data: null,
+        };
+      }
+      return {
+        error,
+        data: null,
+      };
+    }
+  }
+}
