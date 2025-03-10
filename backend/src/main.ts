50fd8770b42ba62a369014dac6e7771f54232cf7
José Valentino <josvalmtrz@gmail.com>
2025-03-10 12:50:54 -0500

    feat: enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,26 +1,27 @@
-import { NestFactory, Reflector } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
+import { NestFactory, Reflector } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { SwaggerTheme, SwaggerThemeNameEnum } from "swagger-themes";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const theme = new SwaggerTheme();
+	const app = await NestFactory.create(AppModule);
+	app.enableCors();
+	const theme = new SwaggerTheme();
 
-  const config = new DocumentBuilder()
-    .setTitle('Backoffice Athor')
-    .setDescription('API del backoffice de Athor')
-    .setVersion('1.0')
-    .addBearerAuth()
-    // .addTag('cats')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, documentFactory, {
-    customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK),
-    //TODO: Esto genera el JSON crudo para asi poder importarlo desde donde quiera o imprimir la documentacion
-    // jsonDocumentUrl: "/docs.json"
-  });
+	const config = new DocumentBuilder()
+		.setTitle("Backoffice Athor")
+		.setDescription("API del backoffice de Athor")
+		.setVersion("1.0")
+		.addBearerAuth()
+		// .addTag('cats')
+		.build();
+	const documentFactory = () => SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("docs", app, documentFactory, {
+		customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK),
+		//TODO: Esto genera el JSON crudo para asi poder importarlo desde donde quiera o imprimir la documentacion
+		// jsonDocumentUrl: "/docs.json"
+	});
 
-  await app.listen(process.env.PORT ?? 3000);
+	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
