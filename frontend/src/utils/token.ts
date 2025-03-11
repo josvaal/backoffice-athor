4faf1328fa45f974ed90ea97dd796d09e2e7465d
José Valentino <josvalmtrz@gmail.com>
2025-03-11 12:04:16 -0500

    fix: better token validation

--- frontend/src/utils/token.ts
+++ frontend/src/utils/token.ts
@@ -1,6 +1,11 @@
 export function checkAccessToken() {
 	const token = localStorage.getItem("access_token");
-	if (token && token.trim() !== "") {
+	if (
+		token &&
+		token.trim() !== "" &&
+		token !== undefined &&
+		token !== "undefined"
+	) {
 		return true;
 	}
 	return false;
