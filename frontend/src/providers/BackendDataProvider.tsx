782e5602f61296c9f225c76185231f50300ddf5e
José Valentino <josvalmtrz@gmail.com>
2025-03-13 12:03:45 -0500

    feat: listar usuarios con axios.get

--- frontend/src/providers/BackendDataProvider.tsx
+++ frontend/src/providers/BackendDataProvider.tsx
@@ -14,3 +14,49 @@ export const customDataProvider: DataProvider = {
   getOne: ({ resource, id, meta }) => Promise,
   getApiUrl: () => "",
 };
+
+const getListProvider = async ({
+  resource,
+  pagination,
+  sorters,
+  filters,
+  meta,
+}) => {
+  const { current, pageSize } = pagination ?? {};
+
+  const handleLogout = () => {
+    console.log("Cerrando sesión...");
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("refresh_token");
+    toast.success("Cerrando sesión...");
+    window.location.reload();
+  };
+
+  const ba_url = import.meta.env.VITE_BA_URL;
+  const token = retrieveAccesToken();
+  if (
+    !token ||
+    token.trim() === "" ||
+    token === undefined ||
+    token === "undefined"
+  ) {
+    handleLogout();
+    return;
+  }
+
+  // Adjust request parameters to meet the requirements of your API
+  const response = await axios.get(`${ba_url}/${resource}/list`, {
+    params: { _page: current, _limit: pageSize },
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  // The total row count could be sourced differently based on the provider
+  const total = response.headers["x-total-count"] ?? response.data.length;
+
+  return {
+    data: response.data,
+    total,
+  };
+};
